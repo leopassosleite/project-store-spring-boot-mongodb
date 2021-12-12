b3d48700df0a3529b09dc42bd1af40626c8e7d0e
leopassosleite <leopassos.leite4@gmail.com>
2021-12-12 18:59:57 -0300

    Project created

---
+++ src/main/java/com/xbarts/storemongo/StoremongoApplication.java
@@ -0,0 +1,13 @@
+package com.xbarts.storemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoremongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoremongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xbarts/storemongo/StoremongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbarts.storemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoremongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
