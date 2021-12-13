d9b3ec876779aa4b3038ac2832e90566348431ee
leopassosleite <leopassos.leite4@gmail.com>
2021-12-13 19:21:35 -0300

    Obtendo um usuário por id

--- src/main/java/com/xbarts/storemongo/services/UserService.java
+++ src/main/java/com/xbarts/storemongo/services/UserService.java
@@ -1,13 +1,14 @@
 package com.xbarts.storemongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.xbarts.storemongo.domain.User;
 import com.xbarts.storemongo.repository.UserRepository;
-
+import com.xbarts.storemongo.services.exception.ObjectNotFoundException;
 @Service
 public class UserService {
 
@@ -16,5 +17,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Id não encontrado"));
 	}
 }
