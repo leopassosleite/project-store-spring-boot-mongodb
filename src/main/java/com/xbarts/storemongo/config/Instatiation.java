74728573ac6de6d37e777a4bf651e7b795dfce8d
leopassosleite <leopassos.leite4@gmail.com>
2021-12-14 18:37:54 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/xbarts/storemongo/config/Instatiation.java
+++ src/main/java/com/xbarts/storemongo/config/Instatiation.java
@@ -1,12 +1,16 @@
 package com.xbarts.storemongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.xbarts.storemongo.domain.Post;
 import com.xbarts.storemongo.domain.User;
+import com.xbarts.storemongo.repository.PostRepository;
 import com.xbarts.storemongo.repository.UserRepository;
 
 @Configuration
@@ -15,16 +19,27 @@ public class Instatiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User leonardo = new User(null, "Leonardo", "leopassos.leite@gmail.com");
 		User tailine = new User(null, "Tailine", "tailine.prates@gmail.com");
 		User alice = new User(null, "Alice", "alice.coutinho@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("10/12/2021"), "Produto excelente", "Produto de muita qualidade, recomendo!", leonardo);
+		Post post2 = new Post(null, sdf.parse("14/12/2021"), "Pruduto ruim", "Produdo abaixo de minhas expectativas, não recomendo!", tailine);
+		
 		userRepository.saveAll(Arrays.asList(leonardo, tailine, alice));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
